f4c893f34a194b2003aba2fac4f8091eb3e5c29d
shin2012649 <shin2012649@gmail.com>
2023-09-21 19:02:07 +0900

    FAQ 구현(faq.js,css, html 수정)

--- src/main/resources/static/js/faq.js
+++ src/main/resources/static/js/faq.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log(document.getElementById("faq-list"))
     // 서버에서 FAQ 목록을 가져오는 함수를 호출하고 목록을 생성합니다.
     loadFAQList();
 });
@@ -21,7 +20,17 @@ function loadFAQList() {
                 // FAQ 항목을 클릭할 때 이벤트 리스너를 추가합니다.
                 faqItem.addEventListener("click", function () {
                     const content = this.dataset.content;
-                    alert(content); // 나중에는 모달 또는 다른 방법으로 표시할 수 있습니다.
+
+                    // FAQ 내용이 이미 생성되어 있는 경우에는 숨깁니다.
+                    if (this.classList.contains("active")) {
+                        hideContent();
+                    } else {
+                        // FAQ 내용을 보여줍니다.
+                        showContent(content);
+                    }
+
+                    // FAQ 항목의 활성 상태를 토글합니다.
+                    this.classList.toggle("active");
                 });
 
                 faqList.appendChild(faqItem);
@@ -30,4 +39,24 @@ function loadFAQList() {
         .catch(error => {
             console.error("FAQ 데이터를 가져오는 중 오류 발생:", error);
         });
+}
+
+function showContent(content) {
+    // FAQ 내용을 생성하고 표시합니다.
+    const contentElement = document.createElement("div");
+    contentElement.classList.add("faq-content");
+    contentElement.textContent = content;
+    contentElement.id = "faq-content";
+
+    // FAQ 목록 아래에 콘텐트 요소를 추가합니다.
+    const faqList = document.getElementById("faq-list");
+    faqList.appendChild(contentElement);
+}
+
+function hideContent() {
+    // FAQ 내용을 숨깁니다.
+    const contentElement = document.getElementById("faq-content");
+    if (contentElement) {
+        contentElement.remove();
+    }
 }
