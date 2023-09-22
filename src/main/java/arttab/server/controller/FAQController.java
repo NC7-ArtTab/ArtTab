283d24506e923db2ca3040500be0ca949c5ea68f
shin2012649 <shin2012649@gmail.com>
2023-09-22 13:35:15 +0900

    FAQ 컨트롤러 수정

--- src/main/java/arttab/server/controller/FAQController.java
+++ src/main/java/arttab/server/controller/FAQController.java
@@ -1,36 +1,45 @@
-//package arttab.server.controller;
-//
-//import arttab.server.dao.FAQDao;
-//import arttab.server.service.FAQService;
-//import arttab.server.vo.FAQ;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Controller
-//public class FAQController {
-//    private final FAQService faqService;
-//
-//    @Autowired
-//    public FAQController(FAQService faqService) {
-//        this.faqService = faqService;
-//    }
-//
-//    @GetMapping("/faq")
-//    public String showFAQPage(Model model) {
-//        List<FAQ> faqList = faqService.getAllFAQs();
-//        model.addAttribute("faq-list", faqList);
-//        return "faq";
-//    }
-//
-//    @GetMapping("/api/faq")
-//    @ResponseBody
-//    public List<FAQ> getFAQList() {
-//        return faqService.getAllFAQs();
-//    }
-//}
+package arttab.server.controller;
+
+import arttab.server.service.FAQService;
+import arttab.server.vo.FAQ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class FAQController {
+    private final FAQService faqService;
+
+    @Autowired
+    public FAQController(FAQService faqService) {
+        this.faqService = faqService;
+    }
+
+    @GetMapping("/faq")
+    public String showFAQPage(Model model) {
+        List<FAQ> faqList;
+        try {
+            faqList = faqService.list();
+            model.addAttribute("faq-list", faqList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "faq";
+    }
+
+    @GetMapping("/api/faq")
+    @ResponseBody
+    public List<FAQ> getFAQList() {
+        List<FAQ> faqList = null;
+        try {
+            faqList = faqService.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return faqList;
+    }
+}
