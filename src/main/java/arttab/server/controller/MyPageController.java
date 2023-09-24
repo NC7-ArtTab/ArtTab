4db1836deba712d392eaeea9fb46e8e70f229c88
shin2012649 <shin2012649@gmail.com>
2023-09-25 05:47:42 +0900

    회원정보 수정 페이지 보수

--- src/main/java/arttab/server/controller/MyPageController.java
+++ src/main/java/arttab/server/controller/MyPageController.java
@@ -46,7 +46,6 @@ public class MyPageController {
     public String showProfileForm(Model model, HttpSession session) {
 
         Member loginUser = (Member) session.getAttribute("loginUser");
-
         // 로그인한 사용자 정보를 이용하여 프로필 페이지를 보여줌
         model.addAttribute("loginUser", loginUser);
         return "mypage/profile";
@@ -57,13 +56,10 @@ public class MyPageController {
         try {
             // 세션에서 로그인한 사용자 정보를 가져옴
             Member loginUser = (Member) session.getAttribute("loginUser");
-
             // 로그인한 사용자의 memberNo 설정
             updatedMember.setMemberNo(loginUser.getMemberNo());
-
             // 멤버 정보 업데이트
             memberService.updateMember(updatedMember);
-
             return "redirect:/profile?success=true";
         } catch (Exception e) {
 
@@ -80,16 +76,16 @@ public class MyPageController {
         // 로그인한 사용자의 멤버 번호를 추출
         int loggedInMemberNo = loginUser.getMemberNo();
 
-        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
+//        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
         // 해당 멤버 번호를 기반으로 입찰 정보를 가져옴
         List<Bid> myBidList = myBidService.getMyBidList(loggedInMemberNo);
 
-        log.error("입찰 목록 가져오기>>{}",  myBidService.getMyBidList(loggedInMemberNo));
+//        log.error("입찰 목록 가져오기>>{}",  myBidService.getMyBidList(loggedInMemberNo));
 
         // 입찰 정보를 모델에 추가하여 뷰로 전달
         model.addAttribute("myBidList", myBidList);
 
-        log.error("뷰로 전달");
+//        log.error("뷰로 전달");
 
         return "mypage/mybid";
     }
@@ -103,16 +99,16 @@ public class MyPageController {
         // 로그인한 사용자의 멤버 번호를 추출
         int loggedInMemberNo = loginUser.getMemberNo();
 
-        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
+//        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
         // 해당 멤버 번호를 기반으로 입찰 정보를 가져옴
         List<Pay> myPayList = myPayService.getPurchaseHistory(loggedInMemberNo);
 
-        log.error("구매 목록 가져오기>>{}",  myPayService.getPurchaseHistory(loggedInMemberNo));
+//        log.error("구매 목록 가져오기>>{}",  myPayService.getPurchaseHistory(loggedInMemberNo));
 
         // 입찰 정보를 모델에 추가하여 뷰로 전달
         model.addAttribute("myPayList", myPayList);
 
-        log.error("뷰로 전달");
+//        log.error("뷰로 전달");
 
         return "mypage/mypay";
     }
