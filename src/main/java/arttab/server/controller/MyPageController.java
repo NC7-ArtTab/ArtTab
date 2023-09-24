f79ac2e1579db7a1ad15dea21607846bcd9a59da
shin2012649 <shin2012649@gmail.com>
2023-09-25 05:27:00 +0900

    로그인 유저 멤버 번호별 입찰, 구매 정보 리스트 구현

--- src/main/java/arttab/server/controller/MyPageController.java
+++ src/main/java/arttab/server/controller/MyPageController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -42,46 +43,76 @@ public class MyPageController {
     }
 
     @GetMapping("/profile")
-    public String showProfileForm(Model model,  Member loginUser) throws Exception {
-        model.addAttribute("member", loginUser);
+    public String showProfileForm(Model model, HttpSession session) {
+
+        Member loginUser = (Member) session.getAttribute("loginUser");
+
+        // 로그인한 사용자 정보를 이용하여 프로필 페이지를 보여줌
+        model.addAttribute("loginUser", loginUser);
         return "mypage/profile";
     }
 
     @PostMapping("/update-profile")
-    public String updateProfile(@ModelAttribute("member") Member updatedMember,Member loginUser) {
+    public String updateProfile(@ModelAttribute("member") Member updatedMember, HttpSession session) {
         try {
+            // 세션에서 로그인한 사용자 정보를 가져옴
+            Member loginUser = (Member) session.getAttribute("loginUser");
+
             // 로그인한 사용자의 memberNo 설정
             updatedMember.setMemberNo(loginUser.getMemberNo());
 
             // 멤버 정보 업데이트
             memberService.updateMember(updatedMember);
 
-            return "redirect:/mypage/profile?success=true";
+            return "redirect:/profile?success=true";
         } catch (Exception e) {
-            return "redirect:/mypage/profile?error=true";
+
+            return "redirect:/profile?error=true";
         }
     }
 
 
-
     @GetMapping("/mybid")
-    public String showMyBids(Model model) {
-        int loggedInMemberNo = 5; // 가정한 멤버 번호
+    public String showMyBids(Model model, HttpSession session) {
+        // 세션에서 로그인한 사용자 정보를 가져옴
+        Member loginUser = (Member) session.getAttribute("loginUser");
 
+        // 로그인한 사용자의 멤버 번호를 추출
+        int loggedInMemberNo = loginUser.getMemberNo();
+
+        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
+        // 해당 멤버 번호를 기반으로 입찰 정보를 가져옴
         List<Bid> myBidList = myBidService.getMyBidList(loggedInMemberNo);
 
+        log.error("입찰 목록 가져오기>>{}",  myBidService.getMyBidList(loggedInMemberNo));
+
+        // 입찰 정보를 모델에 추가하여 뷰로 전달
         model.addAttribute("myBidList", myBidList);
+
+        log.error("뷰로 전달");
 
         return "mypage/mybid";
     }
 
-    @GetMapping("/mypay")
-    public String showMyPay(Model model) {
-        int loggedInMemberNo = 5; // 가정한 멤버 번호
 
+    @GetMapping("/mypay")
+    public String showMyPay(Model model, HttpSession session) {
+        // 세션에서 로그인한 사용자 정보를 가져옴
+        Member loginUser = (Member) session.getAttribute("loginUser");
+
+        // 로그인한 사용자의 멤버 번호를 추출
+        int loggedInMemberNo = loginUser.getMemberNo();
+
+        log.error("멤버 번호 추출>>> {}",loginUser.getMemberNo());
+        // 해당 멤버 번호를 기반으로 입찰 정보를 가져옴
         List<Pay> myPayList = myPayService.getPurchaseHistory(loggedInMemberNo);
 
+        log.error("구매 목록 가져오기>>{}",  myPayService.getPurchaseHistory(loggedInMemberNo));
+
+        // 입찰 정보를 모델에 추가하여 뷰로 전달
         model.addAttribute("myPayList", myPayList);
+
+        log.error("뷰로 전달");
 
         return "mypage/mypay";
     }
